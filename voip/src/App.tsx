ad1c2f736f42e1ae969900f8dc9ac55b7fe8283c
John Foley <johnmfoley2003@yahoo.com>
2025-03-07 20:58:28 +0100

    Receive SDP, make offer to local signal server

--- voip/src/App.tsx
+++ voip/src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import AudioCapture from "../lib/audio_capture";
+import MakeConnection from "../lib/connection";
+const getaudio = false;
 
 function App() {
   return (
     <div>
-      <AudioCapture />
+      {getaudio ? <AudioCapture /> : null}
+      <MakeConnection />
     </div>
   );
 }
